40dd2ee44bb32dd91b595e607734ab520fe15767
Aleksandr <GReddyd1990@gmail.com>
2021-06-21 21:52:34 +0300

    Change server. Add ThreadPool

---
+++ 01_web/http-server/src/main/java/ru/netology/Server/Main.java
@@ -0,0 +1,12 @@
+package ru.netology.Server;
+
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    Server server = new Server();
+    server.startServer();
+  }
+}
+
+
